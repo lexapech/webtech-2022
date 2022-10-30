ce444511d9a72a5a6c92a0ffffd76b3f6718ed41
alex <lexa.pech@yandex.ru>
2022-10-30 23:00:20 +0300

    angular

--- lab4/src/app/app.module.ts
+++ lab4/src/app/app.module.ts
@@ -1,18 +1,47 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import * as Rollbar from 'rollbar'; // When using Typescript < 3.6.0.
+// `import Rollbar from 'rollbar';` is the required syntax for Typescript 3.6.x.
+// However, it will only work when setting either `allowSyntheticDefaultImports`
+// or `esModuleInterop` in your Typescript options.
 
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import {
+  Injectable,
+  Injector,
+  InjectionToken,
+  NgModule,
+  ErrorHandler, Inject
+} from '@angular/core';
 import { AppComponent } from './app.component';
+import {RouterOutlet} from "@angular/router";
+
+const rollbarConfig = {
+  accessToken: 'ad5ead79d3a24da9855f883a57ca970c',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+};
+
+@Injectable()
+export class RollbarErrorHandler implements ErrorHandler {
+  constructor(@Inject(RollbarService) private rollbar: Rollbar) {}
+
+  handleError(err:any) : void {
+    this.rollbar.error(err.originalError || err);
+  }
+}
+
+export function rollbarFactory() {
+  return new Rollbar(rollbarConfig);
+}
+
+export const RollbarService = new InjectionToken<Rollbar>('rollbar');
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, RouterOutlet],
+  declarations: [ AppComponent ],
+  bootstrap: [ AppComponent ],
+  providers: [
+    { provide: ErrorHandler, useClass: RollbarErrorHandler },
+    { provide: RollbarService, useFactory: rollbarFactory }
+  ]
 })
 export class AppModule { }
